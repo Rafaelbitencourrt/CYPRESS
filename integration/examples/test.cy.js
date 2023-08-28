de92007d69ca7674cc24303415435b492d65cc27
Rafael Bitencourt <rafaelbitencourrt@gmail.com>
2023-08-28 10:15:03 -0300

    add

--- integration/examples/test.cy.js
+++ integration/examples/test.cy.js
@@ -6,14 +6,18 @@ describe('My first Test', function(){
         //selenium get hit url in browser, cypress get acts like findElements of selenium
         cy.get('.product:visible').should('have.length',4)
         //Parent child chaining
-        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
-
-        cy.get('.products').find('.product').each(($el, index, $list) =>{
+        cy.get('.products').as('productLocator')
+        cy.get('@productLocator').find('.product').eq(2).contains('ADD TO CART').click()
+        console.log('SF')
+        cy.get('@productLocator').find('.product').each(($el, index, $list) =>{
 
             const textVeg = $el.find('h4.product-name').text()
             if(textVeg.includes('Cashews')){
                 cy.wrap($el).find('button').click()
             }
+        })
+        cy.get('.brand').then(function(logoelement){
+            cy.log(logoelement.text())
         })
     })
 })
