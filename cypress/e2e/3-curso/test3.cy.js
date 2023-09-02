9e4b0b918e1ce527b95eedf533afb36a07d0b2b8
Rafael Bitencourt <rafaelbitencourrt@gmail.com>
2023-09-02 08:07:06 -0300

    Dropdoown dinamico

--- cypress/e2e/3-curso/test3.cy.js
+++ cypress/e2e/3-curso/test3.cy.js
@@ -13,7 +13,29 @@ describe('Checkbox', function(){
 
         cy.get('select').select('option2').should('have.value','option2')
 
+        ///Dropdown dinamico
 
+        cy.get('#autocomplete').type('ind')
+
+        cy.get('.ui-menu div').each(($el, index, $list) =>{
+
+           if($el.text()==='India')
+           {
+            cy.wrap($el).click()
+           }
+        })
+        cy.get('#autocomplete').should('have.value','India')
+
+        ///verificando se um elemento esta visisel
+
+        cy.get('#displayed-text').should('be.visible')
+        cy.get('#hide-textbox').click()
+        cy.get('#displayed-text').should('be.not.visible')
+        cy.get('#show-textbox').click()
+        cy.get('#displayed-text').should('be.visible')
+
+        ///radio buttons
+        cy.get('[value="radio2"]').check().should('be.checked')
 
     })
 
