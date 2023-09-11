31082b9d822fa745575b87134d24ce83d0efa010
Rafael Bitencourt <rafaelbitencourrt@gmail.com>
2023-09-11 15:00:28 -0300

    Validação de campos, requisitos minimos

--- cypress/e2e/3-curso/test8-framework.cy.js
+++ cypress/e2e/3-curso/test8-framework.cy.js
@@ -5,16 +5,21 @@ describe('Alerts', function(){
 
 
     before(function(){
+        cy.fixture('example').then(function(data){
+            this.data=data
+        })
         //runs once before all tests in the block
     })
 
     it('Test Ckeckbox', function(){
         cy.visit('https://rahulshettyacademy.com/angularpractice/')
 
-        cy.get('input[name="name"]:nth-child(2)').type('Bob')
-        cy.get('#exampleFormControlSelect1').select('Female')
-        cy.get('select').select('Female')
-
+        cy.get('input[name="name"]:nth-child(2)').type(this.data.name)
+        //cy.get('#exampleFormControlSelect1').select('Female')
+        cy.get('select').select(this.data.sexo)
+        cy.get(':nth-child(4) > .ng-untouched').should('have.value', this.data.name)
+        cy.get('input[name="name"]:nth-child(2)').should('have.attr','minlength','2')
+        cy.get('#inlineRadio3').should('be.disabled')
     })
 
     
