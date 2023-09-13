d035dc2f05a64c69b56ee33fc854cf03de6fdc89
Rafael Bitencourt <rafaelbitencourrt@gmail.com>
2023-09-13 17:35:32 -0300

    add comand

--- cypress/e2e/3-curso/test8-framework.cy.js
+++ cypress/e2e/3-curso/test8-framework.cy.js
@@ -20,6 +20,8 @@ describe('Alerts', function(){
         cy.get(':nth-child(4) > .ng-untouched').should('have.value', this.data.name)
         cy.get('input[name="name"]:nth-child(2)').should('have.attr','minlength','2')
         cy.get('#inlineRadio3').should('be.disabled')
+        cy.get(':nth-child(2) > .nav-link').click()
+        cy.selectProduct('Blackberry')
     })
 
     
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,14 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+//função para buscar produto pelo nome.
+
+Cypress.Commands.add('selectProduct', (productName)=>{
+    cy.get('h4.card-title').each(($el, index, $list) =>{
+        if($el.text().includes(productName))
+        {
+            cy.get('button.btn.btn-info').eq(index).click()
+        }
+    })
+})
